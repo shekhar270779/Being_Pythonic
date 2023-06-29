229bc664397b4814d655ec0c70ed932137ad36bf
Shekhar Pandey <p.shekhar.2707@gmail.com>
2023-06-29 23:43:46 +0530

    "added naming convention in 02_Datatype.py"

---
+++ 02_Datatype.py
@@ -0,0 +1,15 @@
+# naming conventions
+
+# variables: all lower case, words separated by underscore
+# constants: all upper case, words separated by underscore
+# functions: all lower case, words separated by underscore
+# Classes: CapWords
+# modules : all lower case, words separated by underscore
+# packages: all lower case, preferably no underscores
+
+##################################################################
+
+# datatype in python
+
+# Number
+# integers and non-integers
