98acb3ecdf6649ee5392f92871b87d2b806f05bf
Shekhar Pandey <p.shekhar.2707@gmail.com>
2023-07-09 22:04:41 +0530

    10 & 11 new files

---
+++ 10_python_optimization.py
@@ -0,0 +1,152 @@
+# every thing is an object
+import platform
+print(platform.python_implementation())
+print(platform.python_version())
+def f():
+    print("hello")
+
+# f: is name of the function
+# f(): invokes the function
+print(type(f))
+print(hex(id(f)))
+
+f = lambda x : x * 2
+
+print(type(f))
+print(hex(id(f)))
+
+# any object can be assigned to a variable
+# so function can also be assigned to a variable
+# function can also be passed as an argument
+# function can also be returned
+x = f
+print(x(2))
+
+def fun(f):
+    return f(5)
+
+print(f"{fun(f)}")
+
+
+b = int(5)
+print(b)
+
+c = int()
+print(c)
+
+d = int('101', base=2)
+print(d)
+
+# help(int)
+
+print(bin(13))
+
+square = lambda x: x ** 2
+cube = lambda x: x ** 3
+
+def select_fun(f_id):
+    if f_id == 1:
+        return square
+    elif f_id == 2:
+        return cube
+    else:
+        return None
+
+
+fun_choice = int(input("Enter 1 for square and 2 for cube:"))
+math_fun = select_fun(fun_choice)
+print(math_fun(5))
+
+result = select_fun(fun_choice)(5)
+print(result)
+
+def execute_fun(f, n):
+    return f(n)
+
+print(execute_fun(select_fun(fun_choice), 5))
+
+# pre cached (interned) range of integers [-5, 256]
+a = 10
+b = int(10)
+c = int('1010', base=2)
+d = 5 + 5
+print(hex(id(a)), hex(id(b)), hex(id(c)), hex(id(d)))
+
+# some strings are also interned by python
+# not all strings are interned by Python
+# we can force a string to be interned using sys.intern()
+
+a = "hello world"
+b = "hello world"
+print(f"is a and b referring to same address? {a is b}")
+
+import sys
+
+a = sys.intern("@#187 Rahatani")
+b = sys.intern("@#187 Rahatani")
+c = "@#187 Rahatani"
+print(f"{hex(id(a))}, {hex(id(b))}, {hex(id(c))}")
+
+import time
+def compare_intern(n):
+    a = sys.intern("this_is_a_long_string_for_comparison")
+    b = sys.intern("this_is_a_long_string_for_comparison")
+    for i in range(n):
+        if a is b:
+            pass
+
+
+start = time.perf_counter()
+compare_intern(50000)
+end = time.perf_counter()
+print(f"{end - start}")
+
+def compare_string(n):
+    a = "this is a long string for_comparison"
+    b = "this is a long string for comparison"
+    for i in range(n):
+        if a == b:
+            pass
+
+start = time.perf_counter()
+compare_string(50000)
+end = time.perf_counter()
+print(f"{end - start}")
+
+# Set membership is faster than list or tuple membership
+# instead of x in [1,2,3] or x in (1,2,3), write x in {1,2,3}
+
+def my_func():
+    mins = 24 * 60 # gets pre calculated and stored
+    t = (1, 2) * 5 # gets pre calculated and stored
+    x = 'abc' * 3 # gets pre calculated and stored
+    y = 'abcdefghij' * 3 # does not get pre calculated and stored
+    z = ['a', 'b'] * 3
+
+print(my_func.__code__.co_consts)
+
+import string
+
+char_list = list(string.ascii_letters)
+char_tuple = tuple(string.ascii_letters)
+char_set = set(string.ascii_letters)
+
+def membership_test(n, container, e):
+    for i in range(n):
+        if e in container:
+            pass
+
+start = time.perf_counter()
+membership_test(10000, char_list, 'z')
+end = time.perf_counter()
+print(f"list membership test: {end - start}")
+
+start = time.perf_counter()
+membership_test(10000, char_tuple, 'z')
+end = time.perf_counter()
+print(f"tuple membership test: {end - start}")
+
+start = time.perf_counter()
+membership_test(10000, char_set, 'z')
+end = time.perf_counter()
+print(f"set membership test: {end - start}")
---
+++ 11_About_Numbers.py
@@ -0,0 +1,123 @@
+# Numbers
+# Integers : int
+# Rational numbers: p/q , p & q are integers and q is not zero : fractions.Fraction
+# Real numbers : can be represented as float or decimal.Decimal
+# Complex Numbers: complex
+# Boolean truth values : bool : False -> 0, True -> 1
+
+# Integers : int datatype
+# int object in python uses variable number of bits
+
+import sys
+import time
+import math
+
+print(f"memory (bytes) by 0 integer: {sys.getsizeof(0)}")
+print(f"memory (bytes) by 1 integer: {sys.getsizeof(1)}")
+print(f"memory (bytes) by 2^1000 integer: {sys.getsizeof(2**1000)}, no. of bits: {(sys.getsizeof(2**1000) - 24)*8}")
+
+def calc(x: int):
+    for i in range(10000000):
+        x * 2
+
+
+start = time.perf_counter()
+calc(10)
+end = time.perf_counter()
+print(f"{end - start} secs")
+
+start = time.perf_counter()
+calc(2**100)
+end = time.perf_counter()
+print(f"{end - start} secs")
+
+# mathemetical operations
+# int + int => int
+# int - int => int
+# int * int => int
+# int / int => float
+# int ** int => int
+# int // int => floor division
+# int % int =>  modulos
+
+# a = b * (a//b) + (a % b)
+
+floor_value = 135 // 4
+mod_value = 135 % 4
+print(f"135/4: floor:{floor_value}, mod:{mod_value}")
+
+# floor : floor of a real number is the largest integer <=a
+print(f"3.1: {math.floor(3.1)}, 3.99:{math.floor(3.99)}, -3.16:{math.floor(-3.16)}")
+
+floor_value = -135 // 4
+mod_value = -135 % 4
+print(f"-135/4: floor:{floor_value}, mod:{mod_value}")
+
+def equation_value(a,b):
+    return b * (a//b) + (a%b)
+
+print(f"13/4: floor: {13//4}, mod: {13%4}, eq_val: {equation_value(13,4)}")
+print(f"-13/4: floor: {-13//4}, mod: {-13%4}, eq_val: {equation_value(-13,4)}")
+print(f"13/-4: floor: {13//-4}, mod: {13%-4}, eq_val: {equation_value(13,-4)}")
+print(f"-13/-4: floor: {-13//-4}, mod: {-13%-4}, eq_val: {equation_value(-13,-4)}")
+
+print(f"13/4: {13/4}, floor:{math.floor(13/4)}, div:{13//4}, mod:{13%4}, trunc: {math.trunc(13/4)}")
+print(f"-13/4: {-13/4}, floor:{math.floor(-13/4)}, div:{-13//4}, mod:{-13%4}, trunc: {math.trunc(-13/4)}")
+
+# integer constructor and bases
+# int class has 2 constructors
+# int(), int(string, base) : string that can be parsed to a number
+
+a = "1110101"
+int_a = int(a, base=2)
+bin_a = bin(int_a)
+print(f"int: {int_a}, bin: {bin_a}")
+
+a = "567"
+int_a = int(a, base=8)
+oct_a = oct(int_a)
+print(f"int: {int_a}, oct: {oct_a}")
+
+a = "abcde"
+int_a = int(a, base=16)
+hex_a = hex(int_a)
+print(f"int: {int_a}, hex: {hex_a}")
+
+def f(n, b):
+    '''custom convertor function to any base
+    '''
+    if b < 2 or b > 36:
+        raise ValueError("base must be between 2 to 36")
+    sign = -1 if n < 0 else 1
+    n = n * sign
+    if n == 0:
+        return '0'
+    lst = ['-'] if sign == -1 else []
+    map = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    x = n
+    while(x > 0):
+        x, m = divmod(x, b)
+        lst.insert(1, map[m])
+
+    return ''.join(lst)
+
+
+print(f(232, 5), int(f(232,5), base=5))
+num = 1096
+base = 15
+print(f"base {base}: {f(num, base)}, int: {int(f(num,base), base=base)}")
+
+num = -1096
+print(f"Representing {num} in different base as below:")
+for b in range(1, 17):
+    try:
+        print(f"base:{b:2}: {f(num, b)}")
+    except ValueError:
+        print(f"base:{b:2}: base should be >= 2")
+        continue
+
+
+
+
+
+
